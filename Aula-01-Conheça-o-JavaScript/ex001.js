1a82a39f70eaa9c31d681845eef5334fd4c81611
EdileyGomes <ediley.gomes@pucpr.edu.br>
2025-06-09 22:05:06 -0400

    Create ex001.js

---
+++ Aula-01-Conheça-o-JavaScript/ex001.js
@@ -0,0 +1,3 @@
+alert('Olá, Mundo') // cria um alerta na tela, com um texto dentro.
+
+console.log('Olá, Mundo') // usando para criar um texto dentro do console, muito parecido com o "print" do python
