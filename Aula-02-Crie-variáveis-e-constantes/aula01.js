50eb21fd5fdd9a554f50eff55bd0273c3130c5a1
EdileyGomes <ediley.gomes@pucpr.edu.br>
2025-06-10 21:16:36 -0400

    Create aula01.js

---
+++ Aula-02-Crie-variáveis-e-constantes/aula01.js
@@ -0,0 +1,17 @@
+// let -- valores que podem ser substituives!
+// const -- valores que não podem ser substituiveis, e devem ser iniciados com um valor!
+let fruta = "banana";
+fruta 
+console.log(fruta);
+fruta = "maça";
+console.log(fruta)
+const animal = "cachorro";
+console.log(animal)
+//const animal = "giraffa"; // erro, não é possivel retribuir uma constante!
+
+//Comparando Valores
+
+let animal2 = "gato";
+console.log(animal2);
+animal2 == "gato"; // true, simplesmente pq é igual os 2 valores
+
