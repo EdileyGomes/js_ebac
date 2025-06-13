c8fc5f6385fff1919942b07d2616957249d73eb5
EdileyGomes <ediley.gomes@pucpr.edu.br>
2025-06-12 21:59:04 -0400

    2

--- Aula-07-Desenvolva-funções/ex001.js
+++ Aula-07-Desenvolva-funções/ex001.js
@@ -22,12 +22,15 @@ function maiorNumero(num1, num2) {
 
 
 function somarNumeros(num1, num2, num3) {
-    var num1 = document.getElementById("num1").value;
-    var num2 = document.getElementById("num2").value;
-    var num3 = document.getElementById("num3").value;
+    var num1 = document.getElementById('num1').value;
+    var num2 = document.getElementById('num2').value;
+    var num3 = document.getElementById('num3').value;
+    num1 = Number(num1);
+    num2 = Number(num2);
+    num3 = Number(num3);
     var res = document.getElementById("resultado");
     let resultado = num1 + num2 + num3
-    return resultado;
+    return res.innerHTML = `E o Resultado de ${num1} + ${num2} + ${num3} é: <strong>${resultado}</strong>`;;
     
 }
 res.innerHTML = `E o Resultado de ${num1} + ${num2} + ${num3} é: <strong>${somarNumeros(Number(num1), Number(num2), Number(num3))}</strong>`;
